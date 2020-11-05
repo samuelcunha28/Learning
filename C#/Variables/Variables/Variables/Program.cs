8e609d5f3b833287d871bf5eb612ee00829db232
SAMUEL LUCIANO CORREIA DA CUNHA <8160526@estg.ipp.pt>
2020-11-05 21:43:17 +0000

    begginer course - variables

---
+++ C#/Variables/Variables/Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "John";
+            int characterAge = 35;
+
+            Console.WriteLine("There once was a man named " + characterName);
+            Console.WriteLine("He was " + characterAge +" years old");
+            // characterName = "Mike";
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("But didn't like being " + characterAge);
+
+            Console.ReadLine();
+        }
+    }
+}
