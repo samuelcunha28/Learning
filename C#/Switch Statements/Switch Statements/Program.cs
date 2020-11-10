262ef1d858908a271a9cb37653cc1384686ce923
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2020-11-10 17:57:36 +0000

    simple switch statement program

---
+++ C#/Switch Statements/Switch Statements/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Switch_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetDay(0));
+        }
+
+        static string GetDay(int dayNum)
+        {
+            string dayName = null;
+
+            switch(dayNum)
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+                case 1:
+                    dayName = "Monday";
+                    break;
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+                case 4:
+                    dayName = "Thursday";
+                    break;
+                case 5:
+                    dayName = "Friday";
+                    break;
+                case 6:
+                    dayName = "Saturday";
+                    break;
+                default:
+                    Console.WriteLine("Invalid day number");
+                    break;
+            }
+
+            return dayName;
+        }
+    }
+}
