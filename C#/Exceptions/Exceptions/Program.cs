9d059c070db8d52dd169ada86adbb5f128e3fed9
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2020-11-10 18:39:49 +0000

    exceptions

---
+++ C#/Exceptions/Exceptions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter another number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+            } catch (Exception e)
+            {
+                Console.WriteLine("Error! " + e.Message);
+            } finally
+            {
+            }
+        }
+    }
+}
