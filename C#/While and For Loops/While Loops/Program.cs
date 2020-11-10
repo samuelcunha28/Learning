5fbccb99bd5076bca851277fddc5496aa2d416e0
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2020-11-10 18:25:03 +0000

    a for loop

--- C#/While and For Loops/While Loops/Program.cs
+++ C#/While and For Loops/While Loops/Program.cs
@@ -6,6 +6,7 @@ namespace While_Loops
     {
         static void Main(string[] args)
         {
+            /*
             int index = 1;
 
             while(index <= 5) 
@@ -20,6 +21,13 @@ namespace While_Loops
                 Console.WriteLine(index2);
                 index2++;
             } while (index2 <= 5);
+            */
+            int[] numbers = { 1, 5, 6, 13, 56 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
         }
     }
 }
