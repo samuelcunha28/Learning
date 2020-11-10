032c7ddd921dae83c30112def074017c0ed15c7a
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2020-11-10 17:15:38 +0000

    a simple calculator program

---
+++ C#/Calculator/Calculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            } else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            } else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            } else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            } else
+            {
+                Console.WriteLine("Invalid operator");
+            }
+        }
+    }
+}
