6f728699fa838635be2e1d3db4eef41e677cd06a
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2020-11-10 18:19:37 +0000

    simple guess game

---
+++ C#/Guessing Game/Guessing Game/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Guessing_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "giraffe";
+            string guess = null;
+            int counter = 0;
+            int limit = 3;
+            bool reachLimit = false;
+
+            while(guess != secretWord && !reachLimit)
+            {
+                if (counter < limit)
+                {
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine();
+                    counter++;
+                } else
+                {
+                    reachLimit = true;
+                }
+            }
+
+            if (reachLimit)
+            {
+                Console.WriteLine("You lose! The secret word was: " + secretWord);
+            } else
+            {
+                Console.WriteLine("You win!");
+            }   
+        }
+    }
+}
