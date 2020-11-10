382d42200c6f435a004a458e1ddc06270dacf48c
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2020-11-10 17:02:31 +0000

    more conditions examples

--- C#/Conditions/Conditions/Program.cs
+++ C#/Conditions/Conditions/Program.cs
@@ -6,6 +6,8 @@ namespace Conditions
     {
         static void Main(string[] args)
         {
+            /*
+             * EXAMPLE OF CONDITIONS
             bool isMale = true;
             bool isTall = true;
 
@@ -22,6 +24,47 @@ namespace Conditions
             {
                 Console.WriteLine("You are not male and not tall");
             }
+            */
+
+            // Console.WriteLine(GetMax(20, 10));
+            // Console.WriteLine(GetMax(2, 10, 30));
+
+        }
+        static int GetMax(int num1, int num2)
+        {
+            int result = 0;
+
+            if (num1 > num2)
+            {
+                Console.WriteLine("Number " + num1 + " is the biggest number");
+                result = num1; 
+            } else
+            {
+                Console.WriteLine("Number " + num2 + " is the biggest number");
+                result = num2;
+            }
+
+            return result;
+        }
+        static int GetMax(int num1, int num2, int num3)
+        {
+            int result = 0;
+
+            if (num1 >= num2 && num1 >= num3)
+            {
+                Console.WriteLine("Number " + num1 + " is the biggest number");
+                result = num1;
+            } else if (num2 >= num1 && num2 >= num3)
+            {
+                Console.WriteLine("Number " + num2 + " is the biggest number");
+                result = num2;
+            } else
+            {
+                Console.WriteLine("Number " + num3 + " is the biggest number");
+                result = num3;
+            }
+
+            return result;
         }
     }
 }
