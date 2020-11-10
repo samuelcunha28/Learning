65181802118943eac6669b65bb97ba2873181407
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2020-11-10 18:05:08 +0000

    while loops

---
+++ C#/While Loops/While Loops/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace While_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 1;
+
+            while(index <= 5) 
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+
+            int index2 = 6;
+            do
+            {
+                Console.WriteLine(index2);
+                index2++;
+            } while (index2 <= 5);
+        }
+    }
+}
