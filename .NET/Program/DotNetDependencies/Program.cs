7a9addd203415f4d36b097b713bc041ec20c95a1
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2020-11-15 19:08:02 +0000

    web api with mongo

--- .NET/Program/DotNetDependencies/Program.cs
+++ .NET/Program/DotNetDependencies/Program.cs
@@ -19,11 +19,29 @@ namespace DotNetDependencies
         }
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("Quantities:");
             HumanizeQuantities();
 
             Console.WriteLine("\nDate/Time Manipulation");
             HumanizeDates();
+            */
+            int result = Fibonacci(5);
+            Console.WriteLine(result);
+            
+        }
+
+        static int Fibonacci(int n) {
+            int n1 = 0;
+            int n2 = 1;
+            int sum = 0;
+
+            for (int i = 2; i < n; i++) {
+                sum = n1 + n2;
+                n1 = n2;
+                n2 = sum;
+            }
+            return n == 0 ? n1 : n2;
         }
     }
 }
