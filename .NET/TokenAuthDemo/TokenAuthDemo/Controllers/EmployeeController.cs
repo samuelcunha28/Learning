70d1b91980a1cbadfbd48de8635dad48a6e6a67a
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2020-11-25 12:47:26 +0000

    token authentication working

---
+++ .NET/TokenAuthDemo/TokenAuthDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TokenAuthDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class EmployeeController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok(new string[] { "Employee one", "Employee two", "Employee three" });
+        }
+    }
+}
